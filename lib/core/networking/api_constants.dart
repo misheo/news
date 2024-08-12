dfc69bf7d345f38b8c139d75d14fec93f411221e
Said Nassar <saidnassar18@gmail.com>
2024-08-12 20:50:46 +0300

    initial the app

---
+++ lib/core/networking/api_constants.dart
@@ -0,0 +1,16 @@
+class ApiConstants{ 
+  static String baseUrl = 'https://newsapi.org/v2';
+  static String apiKey = 'a6a2b0a9a6b84b7f8b8f8b8f8b8f8b8f';
+
+  static String topHeadlines = '/top-headlines';
+
+  static String everything = '/everything';
+
+  static String sources = '/sources';
+
+  static String search = '/everything';
+
+
+
+
+}
