435914e4fc4f75273f2f13a83c688ecc093416ad
Said Nassar <saidnassar18@gmail.com>
2024-08-24 04:05:17 +0300

    Refactor Category Screen UI and Add Localized Category Names
    - Integrated `flutter_screenutil` for responsive UI design in the CategoryScreen.
    - Updated the AppBar title to use localized text for categories.
    - Enhanced the Text widget in the body to be responsive and added text overflow handling.
    - Introduced a SizedBox for spacing improvements.
    - Modified CategoryGridWidget to dynamically display categories using a list of maps containing category details.
    - Added new localized strings for category names in the English localization file.
    - Refactored CategoryWidget to improve layout and responsiveness, ensuring better alignment and readability.
    
    This commit enhances the user interface of the Category Screen, making it more responsive and localized, while also improving the overall layout and readability of category widgets.

--- lib/features/categorys/ui/category_screen.dart
+++ lib/features/categorys/ui/category_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'widgets/category_grid_widget.dart';
 
@@ -12,14 +13,19 @@ class CategoryScreen extends StatelessWidget {
     return Scaffold(
       // body: Center(child: Text("Category Screen")),
       appBar: AppBar(
-        title: const Text("Category Screen"),
+        title:  Text(AppLocalizations.of(context)?.categories ?? ''),
       ),
       body: Column(
         children: [
+          SizedBox(height: 20.h),
           Text(
             AppLocalizations.of(context)?.pick_categories ?? '',
             maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.start,
           ),
+          SizedBox(height: 20.h),
           Expanded(child: CategoryGridWidget())
         ],
       ),
--- lib/features/categorys/ui/widgets/category_widget.dart
+++ lib/features/categorys/ui/widgets/category_widget.dart
@@ -14,22 +14,31 @@ class CategoryWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container( 
-      margin: const EdgeInsets.only(right: 10),
-      child: Column(
-        children: [
-          Container(
-            height: 50.h,
-            width: 50.w,
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: color,
+        border: Border.all(color: const Color.fromARGB(255, 255, 255, 255), width: 1),
+
+      ),
+      margin:  EdgeInsets.symmetric(horizontal: 10.w , vertical: 5.h),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 50.h,
+              width: 50.w,
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: Image.asset(image , height: 80.h, ),
+              ),
             ),
-            child: Center(
-              child: Image.asset(image),
-            ),
-          ),
-          Text(category),
-        ],
+            Text(category , style: TextStyle(color: Colors.white, fontSize: 25.sp),),
+          ],
+        ),
       ),
     );
   }
